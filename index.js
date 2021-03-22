3d8c449239579b1d2f4cd80e339c5e7bceb6d970
hkgnp <ben@pngs.cc>
2021-03-22 17:06:21 +0800

    -

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-const express = require("express");
-const hbs = require("hbs");
-const wax = require("wax-on");
-require("dotenv").config();
+const express = require('express');
+const hbs = require('hbs');
+const wax = require('wax-on');
+require('dotenv').config();
 const cors = require('cors');
-const landingRoutes = require('./routes/landing')
-const corporateRoutes = require('./routes/corporate')
+const landingRoutes = require('./routes/landing');
+const corporateRoutes = require('./routes/corporate');
 
 // create an instance of express app
 let app = express();
 
 // set the view engine
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 
 // static folder
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // setup wax-on
 wax.on(hbs.handlebars);
-wax.setLayoutPath("./views/layouts");
+wax.setLayoutPath('./views/layouts');
 
 // enable forms
 app.use(
-    express.urlencoded({
-        extended: false
-    })
+  express.urlencoded({
+    extended: false,
+  })
 );
 
 (async () => {
-    app.use('/yarn add ', landingRoutes)
-    app.use('/company', corporateRoutes)
+  app.use('/', landingRoutes);
+  app.use('/company', corporateRoutes);
 })();
 
-
-
 app.listen(3000, () => {
-    console.log("Server has started");
+  console.log('Server has started');
 });
